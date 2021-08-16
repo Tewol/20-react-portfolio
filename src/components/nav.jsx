b976435ba8b786d225bf0b8bd66cbdd8368fd372
Hewan Redie <tewol22@gmail.com>
2021-08-16 15:07:33 -0500

    Link fix.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from "react-router-dom"
 
 const HeaderNav = () => {
   return (
     <div className="NavBar">
       <Nav className="nav nav-tabs">
-
         <NavItem className="nav-item">
-          <NavLink className="LinkList" href="/About">
+          <Link className="LinkList" to="/about">
             About
-          </NavLink>
+          </Link>
         </NavItem> 
 
         <NavItem className="nav-item">
-          <NavLink className="LinkList" href="/Portfolio">
+          <NavLink className="LinkList" href="/portfolio">
             Portfolio
           </NavLink>
         </NavItem>
 
         <NavItem className="nav-item">
-          <NavLink className="LinkList" href="/Contact">
+          <NavLink className="LinkList" href="/contact">
             Contact
           </NavLink>
         </NavItem>
 
         <NavItem className="nav-item">
-          <NavLink className="LinkList" href="/Resume">
+          <NavLink className="LinkList" href="/resume">
             Resume
           </NavLink>
         </NavItem>
-
       </Nav>
     </div>
   );
