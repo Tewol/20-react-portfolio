36a4e64725b90c4124581a65aa47b913e2b73ff5
Hewan Redie <tewol22@gmail.com>
2021-08-17 20:56:25 -0500

    final touch

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,89 +1,80 @@
-import React, { useState } from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import '../../App.css';
-import { validateEmail } from '../../utils/helpers';
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { validateEmail } from "../../utils/helpers";
 
 const ContactForm = (props) => {
-
-  const [fullName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [fullName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-
     //Set the state to email, username, and message based on the input type,
-    if (inputType === 'fullName') {
+    if (inputType === "fullName") {
       setUserName(inputValue);
-    } else if (inputType === 'email') {
+    } else if (inputType === "email") {
       setEmail(inputValue);
-    }  else {
+    } else {
       setMessage(inputValue);
     }
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    
     if (!validateEmail(email)) {
-      setErrorMessage('Invalid Email');
+      setErrorMessage("wrong email");
       return;
     }
-
-    setUserName('');
-    setEmail('');
-    setMessage('');
-    
+    setUserName("");
+    setEmail("");
+    setMessage("");
   };
 
   return (
-    <Form className="contact" >
+    <Form className="contact">
       <div>
-      <h3> Contact:</h3>
-      <FormGroup className="label">
-        <Label for="name"> Full Name: </Label>
-        <Input 
-          type="text"
-          name="fullName"
-          placeholder="Hewan Redie"
-          value={fullName}
-          onChange={handleInputChange}/>
-      </FormGroup>
-      
-      <FormGroup>
-        <Label for="email"> Email Address: </Label>
-        <Input 
-          type="email"
-          name="email"
-          placeholder="example@example.com"
-          value={email}
-          onChange={handleInputChange}/>
-      </FormGroup>
-
-      <FormGroup>
-        <Label for="message"> Message: </Label>
-        <Input 
-          type="textarea" 
-          name="comment" 
-          value={message}
-          onChange={handleInputChange}/>
-      </FormGroup>
-
-      <Button className="sub"
-        onSubmit={handleFormSubmit}> Submit </Button>
-    </div>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-
-    </Form> 
+        <h3> Contact:</h3>
+        <FormGroup className="label">
+          <Label for="name"> Full Name: </Label>
+          <Input
+            type="text"
+            placeholder="Hewan Redie"
+            value={fullName}
+            onChange={handleInputChange}
+            name="fullName"
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email"> Email Address: </Label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="example@example.com"
+            value={email}
+            onChange={handleInputChange}
+            onSubmit={handleFormSubmit}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="message"> Message: </Label>
+          <Input
+            type="textarea"
+            name="comment"
+            value={message}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
+        <Button className="sub"> Submit </Button>
+      </div>
+    </Form>
   );
-}
+};
 
 export default ContactForm;
